f0194e52a57a9238712a9ecdb076908872966499
DAVID BETANCUR ECHAVARRIA <davidbetancur262776@correo.itm.edu.co>
2023-11-12 10:27:20 -0500

    creation and configuration of the database and creation of the table

---
+++ Parcial3_BetancurEchavarriaDavid/DAL/DataBaseContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial3_BetancurEchavarriaDavid.DAL.Entities;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Reflection.Emit;
+
+namespace Parcial3_BetancurEchavarriaDavid.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
+        // Se mapea la identidad para convertirla en una tabla
+        public DbSet<Ticket> Tickets { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Ticket>().HasIndex(c => c.Id).IsUnique();
+
+        }
+
+
+    }
+}
