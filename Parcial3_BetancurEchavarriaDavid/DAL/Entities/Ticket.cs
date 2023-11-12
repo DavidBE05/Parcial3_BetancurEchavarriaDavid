f50e1c9a76cb506475f48e97d5de7f6851545e5a
DAVID BETANCUR ECHAVARRIA <davidbetancur262776@correo.itm.edu.co>
2023-11-12 09:38:51 -0500

    creation of the ticket class

---
+++ Parcial3_BetancurEchavarriaDavid/DAL/Entities/Ticket.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial3_BetancurEchavarriaDavid.DAL.Entities
+{
+    public class Ticket
+    {
+        #region Properties
+        [Key]
+        public virtual Guid Id { get; set; }
+
+        [Display(Name = "Fecha de Uso")]
+        public virtual DateTime? UseDate { get; set; }
+
+        [Display(Name = "Se uso?")]
+        public bool? IsUsed { get; set; }
+
+        [Display(Name = "Porteria de Entrada")]
+        [MaxLength(500, ErrorMessage = "El campo {0} debe ser de {1} caracteres.")]
+        public string? EntranceGate { get; set; }
+        #endregion
+    }
+}
